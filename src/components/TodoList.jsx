eb1fdf2a2f1ccafe03fcb6af836180a7ee1ec3de
Prisca <priscamasereli@gmail.com>
2025-08-29 10:31:04 +0200

    Feat: add mark task as complete functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,80 @@
-import { useSelector } from "react-redux";
-import TodoItem from "./TodoItem";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodo, deleteTodo, editTodo } from "../Redux/todosSlice";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos.list);
+  const dispatch = useDispatch();
 
-  if (todos.length === 0) {
-    return (
-      <tr>
-        <td colSpan="3" className="text-center text-gray-500 italic py-4">
-          No tasks yet. Start by adding one!
-        </td>
-      </tr>
-    );
-  }
+  // Track which todo is being edited
+  const [editingId, setEditingId] = useState(null);
+  const [newText, setNewText] = useState("");
+
+  const handleEdit = (id, currentText) => {
+    setEditingId(id);
+    setNewText(currentText);
+  };
+
+  const handleSave = (id) => {
+    if (newText.trim()) {
+      dispatch(editTodo({ id, newText }));
+    }
+    setEditingId(null);
+    setNewText("");
+  };
+
   return (
     <>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <tr key={todo.id} className="border-b">
+          {/* Task column (checkbox + text OR input when editing) */}
+          <td className="px-4 py-2 flex items-center gap-2">
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggleTodo(todo.id))}
+            />
+            {editingId === todo.id ? (
+              <input
+                type="text"
+                value={newText}
+                onChange={(e) => setNewText(e.target.value)}
+                className="border p-1 rounded w-full"
+              />
+            ) : (
+              <span
+                className={todo.completed ? "line-through text-gray-500" : ""}
+              >
+                {todo.text}
+              </span>
+            )}
+          </td>
+
+          {/* Actions column */}
+          <td className="px-4 py-2 text-right space-x-3">
+            {editingId === todo.id ? (
+              <button
+                onClick={() => handleSave(todo.id)}
+                className="text-green-500"
+              >
+                Save
+              </button>
+            ) : (
+              <button
+                onClick={() => handleEdit(todo.id, todo.text)}
+                className="text-blue-500"
+              >
+                Edit
+              </button>
+            )}
+            <button
+              onClick={() => dispatch(deleteTodo(todo.id))}
+              className="text-red-500"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
       ))}
     </>
   );
